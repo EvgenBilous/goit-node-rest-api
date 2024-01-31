548158cce4bcf6ea33fd6e823e219bcdb52f4e00
Eugene <131405736+EvgenBilous@users.noreply.github.com>
2024-01-31 12:24:50 -0600

    fix required

--- schemas/contactsSchemas.js
+++ schemas/contactsSchemas.js
@@ -2,8 +2,8 @@ import Joi from 'joi';
 
 export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(30).required(),
-  phone: Joi.string().min(5).max(10),
-  email: Joi.string().email(),
+  phone: Joi.string().min(5).max(10).required(),
+  email: Joi.string().email().required(),
 });
 
 export const updateContactSchema = Joi.object({
