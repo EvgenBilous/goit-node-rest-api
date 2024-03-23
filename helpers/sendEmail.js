4fdac584d3cb3e4cfae6af87d45a97dedcd0bb38
Eugene <131405736+EvgenBilous@users.noreply.github.com>
2024-03-23 11:04:32 -0500

    fix sendgrid

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,49 +1,13 @@
-import fs from 'fs/promises';
-import path from 'path';
-import nodemailer from 'nodemailer';
-import { User } from '../models/userModels.js';
-import { catchAsync } from './catchAsync.js';
-const { BASE_URL, EMAIL_FROM, EMAIL_PASSWORD } = process.env;
+import dotenv from 'dotenv';
+dotenv.config();
+import sendgrid from '@sendgrid/mail';
 
-const nodemailerConfig = {
-  service: 'gmail',
-  auth: {
-    user: EMAIL_FROM,
-    pass: EMAIL_PASSWORD,
-  },
+const { SENDGRID_API_KEY, EMAIL_FROM } = process.env;
+
+sendgrid.setApiKey(SENDGRID_API_KEY);
+
+export const sendEmail = async data => {
+  const email = { ...data, from: EMAIL_FROM };
+  await sendgrid.send(email);
+  return true;
 };
-
-let transporter = nodemailer.createTransport(nodemailerConfig);
-
-export const sendEmail = async tokenVerify => {
-  var message = {
-    from: '',
-    to: 'receiver@sender.com',
-    subject: 'Message title',
-    text: 'Plaintext version of the message',
-    html: `<a href="http://localhost:3000/api/users/verify/${tokenVerify}">Click to verify email</a>`,
-  };
-
-  await fs.writeFile(path.resolve('tmp', 'verifyURL'), message.html, 'utf-8');
-
-  // transporter.sendMail(message);
-};
-
-export const resendVerifyEmail = catchAsync(async (req, res) => {
-  const { email } = req.body;
-  const user = await User.findOne({ email });
-  console.log(BASE_URL);
-  if (!user) throw HttpError(401, 'Email not found');
-
-  if (user.verify) throw HttpError(400, 'Verification has already been passed');
-
-  const verifyEmail = {
-    to: email,
-    subject: 'Verify your email',
-    html: `<a target="_blank" href="${BASE_URL}/users/verify/${user.verificationToken}">Click here to verify email</a>`,
-  };
-
-  await sendEmail(verifyEmail);
-
-  res.json({ message: 'Verification email sent' });
-});
